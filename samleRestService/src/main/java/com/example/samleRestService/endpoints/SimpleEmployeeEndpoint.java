68a106af325d03751a652bcbfa2b0fd3975f5f4b
Nilavalagan Sugumaran <>
2020-04-24 11:02:51 +0100

    Added all HTTP verbs

--- samleRestService/src/main/java/com/example/samleRestService/endpoints/SimpleEmployeeEndpoint.java
+++ samleRestService/src/main/java/com/example/samleRestService/endpoints/SimpleEmployeeEndpoint.java
@@ -1,8 +1,7 @@
 package com.example.samleRestService.endpoints;
 
 import com.example.samleRestService.resources.Employee;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController // To expose endpoints or apis
 public class SimpleEmployeeEndpoint {
@@ -12,4 +11,36 @@ public class SimpleEmployeeEndpoint {
 
         return new Employee(101L,"Nila-101", 1000.00d);
     }
+
+    // http://localhost:9002/simpleEmployee/102
+    @GetMapping(path = "/simpleEmployee/{id}")
+    public Employee getEmployeeByID(@PathVariable("id") Long id ) {
+
+        return null;
+    }
+
+
+    // http://localhost:9002/simpleEmployee?empId=102
+    @DeleteMapping(path = "/simpleEmployee")
+    public void deleteAndEmployee(@RequestParam("empId") Long id) {
+
+
+    }
+
+    // http://localhost:9002/simpleEmployee // need to pass request body
+    @PostMapping(path = "/simpleEmployee")
+    public Employee createEmployee(@RequestBody Employee newEmployee) {
+
+
+        return null;
+    }
+
+    // http://localhost:9002/simpleEmployee/{id} // need to pass request body
+    @PutMapping(path = "/simpleEmployee/{id}")
+    public void updateEmployee(@PathVariable("id") Long id, @RequestBody Employee updatedEmployee) {
+
+
+    }
+
+
 }
