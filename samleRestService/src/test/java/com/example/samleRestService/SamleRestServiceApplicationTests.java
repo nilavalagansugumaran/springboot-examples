a1d0174a1a38f24593092d459f784bd3e20a9dbb
Nilavalagan Sugumaran <>
2020-04-24 10:20:47 +0100

    Project set-up

---
+++ samleRestService/src/test/java/com/example/samleRestService/SamleRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samleRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamleRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
