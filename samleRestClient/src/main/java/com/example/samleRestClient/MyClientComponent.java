4c4ae89e9b50b795de1f40a6474d247bcf7d3b7e
Nilavalagan Sugumaran <>
2020-04-24 12:18:09 +0100

    Rest client example

--- samleRestClient/src/main/java/com/example/samleRestClient/MyClientComponent.java
+++ samleRestClient/src/main/java/com/example/samleRestClient/MyClientComponent.java
@@ -1,8 +1,13 @@
 package com.example.samleRestClient;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,18 +15,39 @@ import javax.annotation.PostConstruct;
 
 @Component
 @Data
+@Slf4j
 public class MyClientComponent {
 
     @Value("${rest.service.url:http://dummy-default-value}")
     private String serviceUrl;
 
-    @Autowired private RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
     public void invokeService(){
 
+        // To create a resource
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/xml");
+        HttpEntity<String> entity = new HttpEntity<>("<Employee>\n <id>9102</id>" +
+                "<name>nila-9102</name>\n" +
+                "<salary>9000.0</salary>\n" +
+                " </Employee>", headers);
+        ResponseEntity<String>  response = restTemplate.exchange(serviceUrl, HttpMethod.POST, entity, String.class);
+
+        log.debug("response {}, response body{}", response.getStatusCode(), response.getBody());
+
+
+        // To read a resource
+        HttpHeaders headerOther = new HttpHeaders();
+        headerOther.add("Content-Type", "application/json");
+        headerOther.add("Accept", "application/json");
+        HttpEntity<String> getEntity = new HttpEntity<>("", headerOther);
+        response = restTemplate.exchange(serviceUrl+9102, HttpMethod.GET, getEntity, String.class);
+
+        log.debug("response {}, response body{}", response.getStatusCode(), response.getBody());
 
     }
-    
+
     @PostConstruct
     public void setUp(){
 
--- samleRestClient/src/main/java/com/example/samleRestClient/SamleRestClientApplication.java
+++ samleRestClient/src/main/java/com/example/samleRestClient/SamleRestClientApplication.java
@@ -2,12 +2,16 @@ package com.example.samleRestClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SamleRestClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SamleRestClientApplication.class, args);
+
+		ApplicationContext context = SpringApplication.run(SamleRestClientApplication.class, args);
+
+		context.getBean(MyClientComponent.class).invokeService();
 	}
 
 }
